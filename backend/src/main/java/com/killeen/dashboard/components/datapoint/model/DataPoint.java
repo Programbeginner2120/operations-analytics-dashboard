01c09f40f3d7e693ef7132c5bc2d06abc5736e33
Matthew Killeen <Matthewkilleen99@gmail.com>
2025-12-31 17:57:15 -0500

    added usage of metric in data point and data query

--- backend/src/main/java/com/killeen/dashboard/components/datapoint/model/DataPoint.java
+++ backend/src/main/java/com/killeen/dashboard/components/datapoint/model/DataPoint.java
@@ -3,11 +3,13 @@ package com.killeen.dashboard.components.datapoint.model;
 import java.time.LocalDateTime;
 import java.util.Map;
 
+import com.killeen.dashboard.components.datasource.model.Metric;
+
 import lombok.Data;
 
 @Data
 public class DataPoint<E> {
-    private String metricName;
+    private Metric metric;
     private E value;
     private LocalDateTime timestamp;
     private Map<String, Object> metadata;
--- backend/src/main/java/com/killeen/dashboard/components/dataquery/model/DataQuery.java
+++ backend/src/main/java/com/killeen/dashboard/components/dataquery/model/DataQuery.java
@@ -4,13 +4,15 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
+import com.killeen.dashboard.components.datasource.model.Metric;
+
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class DataQuery {
-    private List<String> metrics;
+    private List<Metric> metrics;
     private LocalDateTime startDate;
     private LocalDateTime endData;
     private Map<String, String> filters;
