e8634b2b3b6d1ab8bb91156ed91498001a6cd3a7
Matthew Killeen <Matthewkilleen99@gmail.com>
2026-01-11 16:10:40 -0500

    got basic example of chart going

--- frontend/src/app/components/dashboard/dashboard-layout/dashboard-layout.component.ts
+++ frontend/src/app/components/dashboard/dashboard-layout/dashboard-layout.component.ts
@@ -1,11 +1,13 @@
 import { Component, computed, inject, Signal, signal, WritableSignal } from "@angular/core";
 import { DashboardCard } from "../../../interfaces/dashboard.interface";
 import { DashboardService } from "../../../services/dashboard.service";
+import { BarChartComponent } from "../../charts/bar-chart/bar-chart.component";
 
 @Component({
     selector: 'app-dashboard-layout',
     templateUrl: './dashboard-layout.component.html',
-    styleUrls: ['./dashboard-layout.component.scss']
+    styleUrls: ['./dashboard-layout.component.scss'],
+    imports: [BarChartComponent]
 })
 export class DashboardLayoutComponent {
     
