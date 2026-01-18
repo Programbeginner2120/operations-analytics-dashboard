cfe201fb4a1c2a6b5c9ce6f34ce2ae580a3c7be8
Matthew Killeen <Matthewkilleen99@gmail.com>
2026-01-18 09:54:57 -0500

    modified dashboard.interface.ts

--- frontend/src/app/interfaces/dashboard.interface.ts
+++ frontend/src/app/interfaces/dashboard.interface.ts
@@ -7,10 +7,42 @@ export enum DashboardVisualizationType {
     PIE_CHART = 'PIE_CHART',
 }
 
+/**
+ * Base transformatin config
+ * This is the base config for all data transformations
+ * Taking in a method string here for now, may change later
+ */ 
+export interface DataTransformConfig {
+    method: string;
+}
+
+/**
+ * Data query config
+ * This is the config for the data query
+ * Taking in a start date and end date for now, will expand later
+ */
+export interface DataQueryConfig {
+    startDate: string;
+    endDate: string;
+}
+
+/**
+ * Dashboard card
+ * This is the interface for a dashboard card
+ * It contains the card's title, data source type,
+ * visualization type, query config, transform config, raw data, 
+ * and transformed data.
+ * Will likely expand later to not have a shitty "any" type for the 
+ * raw and transformed data, maybe using generics later but for now
+ * it's fine.
+ */
 export interface DashboardCard {
     id: number;
     title: string;
     dataSourceType: DashboardDataSourceType;
     visualizationType: DashboardVisualizationType;
-    data?: any[];
+    queryConfig: DataQueryConfig;
+    transformConfig: DataTransformConfig;
+    rawData?: any[];
+    transformedData?: any[];
 }
