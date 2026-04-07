36271ca3b970da4986988f7241fba709af4cff49
Matthew Killeen <Matthewkilleen99@gmail.com>
2026-04-07 06:26:12 -0400

    implemented hamburger component

---
+++ frontend/src/app/shared/components/hamburger/hamburger.component.ts
@@ -0,0 +1,26 @@
+import { Component, signal } from "@angular/core";
+import { LucideAngularModule, Menu, X } from "lucide-angular";
+import { ɵEmptyOutletComponent } from "@angular/router";
+
+@Component({
+    selector: 'app-hamburger-menu',
+    templateUrl: './hamburger.component.html',
+    styleUrls: ['./hamburger.component.scss'],
+    imports: [LucideAngularModule, ɵEmptyOutletComponent]
+})
+export class HamburgerMenuComponent {
+
+    readonly menu = Menu;
+    readonly x = X;
+
+    readonly isMenuOpen = signal<boolean>(false);
+
+    toggleMenu(): void {
+        this.isMenuOpen.update(open => !open);
+    }
+
+    close(): void {
+        this.isMenuOpen.set(false)
+    }
+
+}
